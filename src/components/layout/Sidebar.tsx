77dc21b66780702ac4ac31c48d314cb1980cf553
alima <alima@DESKTOP-LE5SPSM>
2026-06-28 22:06:06 +0300

    Update social links for Instagram and TikTok

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -95,11 +95,11 @@ export async function Sidebar() {
               <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm4.64 6.8c-.15 1.58-.8 5.42-1.13 7.19-.14.75-.42 1-.68 1.03-.58.05-1.02-.38-1.58-.75-.88-.58-1.38-.94-2.23-1.5-.99-.65-.35-1.01.22-1.59.15-.15 2.71-2.48 2.76-2.69.01-.03.01-.14-.07-.19-.08-.05-.19-.02-.27 0-.11.03-1.84 1.18-5.2 3.45-.49.33-.94.5-1.34.49-.45-.01-1.3-.25-1.93-.46-.78-.26-1.4-.4-1.35-.85.03-.23.36-.47 1-.74 3.94-1.71 6.57-2.85 7.89-3.4 3.75-1.56 4.53-1.84 5.04-1.85.11 0 .36.03.49.14.11.09.14.22.15.33-.01.07-.01.12-.02.2z" />
             </svg>
           </Link>
-          <Link href="#" className="hover:text-black transition-colors">
+          <Link href="https://www.instagram.com/uncultured.ua?igsh=aDR0ZjZveHczNzk4&utm_source=qr" target="_blank" rel="noopener noreferrer" className="hover:text-black transition-colors">
             <span className="sr-only">Instagram</span>
             <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-6 h-6"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg>
           </Link>
-          <Link href="#" className="hover:text-black transition-colors">
+          <Link href="https://www.tiktok.com/@uncultured_media?_r=1&_t=ZN-97adGKSZIOI" target="_blank" rel="noopener noreferrer" className="hover:text-black transition-colors">
             <span className="sr-only">TikTok</span>
             <svg viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6"><path d="M19.321 5.562a5.122 5.122 0 0 1-3.552-1.332 5.105 5.105 0 0 1-1.36-3.522h-3.442v15.221a3.633 3.633 0 1 1-3.633-3.633 3.59 3.59 0 0 1 1.761.455v-3.765a7.352 7.352 0 0 0-1.761-.212 7.382 7.382 0 1 0 7.382 7.382V8.497a8.55 8.55 0 0 0 4.605 1.341V6.264a5.132 5.132 0 0 1-1.29-.11z" /></svg>
           </Link>
