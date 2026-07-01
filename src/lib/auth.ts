8317d971dce7509d729ffa9f7fe7caa285ea080b
alima <alima@DESKTOP-LE5SPSM>
2026-07-01 13:12:14 +0300

    Fix: Allow dangerous email account linking for Google auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -13,6 +13,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      allowDangerousEmailAccountLinking: true,
     }),
     CredentialsProvider({
       name: "Credentials",
